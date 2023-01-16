65fa636ceed468d681e8df9f518e7c36841d04a8
julia-marinh0 <122797133+julia-marinh0@users.noreply.github.com>
2023-01-16 10:57:01 -0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class Main{
     public static void main(String[] args) {
-        Sytem.out.println("Começamos o projeto!");
+
+        Scanner scan = new Scanner(System.in);
+        String decisao;
+        //print("");
+
+        //pedir o nick
+        //plot da historinha: ser um julgamento a partir do nivel de maldade/bondade, com dois finais em que se for maior que 3 vai pro inferno, e menor que 3 vai pro céu
+        
+        //importar scanner - foi
+        //criar obj jogador
+        //extends classe jogador
+        //string da decisao - foi
+        //if e else
+        //nao esquecer de no final de usar o scanner, fechar ele com: scan.close();
     }
 }
