ed6b84fc1e03c1c14310b62c868b869cc2593bf2
sonalidias <119533286+sonalidias@users.noreply.github.com>
2023-01-16 15:01:49 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,35 +1,83 @@
 import java.util.Scanner;
 
-public class Main{
+public class Main extends Jogador{
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
+        Jogador jogador = new Jogador();
         String decisao;
-        //limpar();
+        limpar();
 
-        System.out.println("Oi, você acabou de morrer...");
-        System.out.println("Neste momento iremos avaliar se você foi uma boa pessoa na Terra. Iremos julgar o seu destino!");
-        System.out.println("Informe o seu nome: ");
-        //jogador.nick = scan.nextLine();
-        //limpar();
-        //System.out.println( + ", nós iremos avaliar as primeiras etapas da sua vida.");
-        System.out.println("\033[3mFLASHBACK da sua infância...\033[0m");
-        System.out.println("Você entra em uma loja de doces, e vê um chocolate. Deseja pegá-lo?");
-        //if/else sim ou nao. sim = aumenta maldade/ nao = aumenta bondade
-        //adicionar pre ado, adolescente e adulto
+        println("Oi, você acabou de morrer...");
+        println("Neste momento iremos avaliar se você foi uma boa pessoa na Terra. Iremos julgar o seu destino!");
+        println("Informe o seu nome: ");
+        jogador.nome = scan.nextLine();
+        limpar();
+        println( jogador.nome + ", nós iremos avaliar as primeiras etapas da sua vida.");
+        println("\033[3mFLASHBACK da sua infância...\033[0m");
+        println("No dia de brincar na escola, todos os seus amigos levaram brinquedos, menos você.");
+        println("Você foi até seu amigo e pediu um brinquedo emprestado, ele recusou.");
+        println("Depois de um tempo ele saiu de sala. Você com raiva foi até o brinquedo. Deseja quebrar?");
+        println("Sim ou Não?");
+        decisao = scan.nextLine();
+        if(decisao.equals("Sim")){
+            jogador.pontoMald();
+            println("Você foi malvado em sua infância, achava que fosse um bom menino.");
+        }else {
+            jogador.pontoBond();
+            println("Você foi um bom menino em sua infância!");
+
+        }
+
         
+        println("\033[3mFLASHBACK da sua pré-adolescência...\033[0m");
+        println("Você entra em uma loja de doces, e vê um chocolate. Deseja pegá-lo?");
+        println("Sim ou Não?");
+        decisao = scan.nextLine();
+        if(decisao.equals("Sim")){
+            jogador.pontoMald();
+            println("Na pré-adolescencia todos são difíceis...");
+        }else {
+            jogador.pontoBond();
+            println("Uma boa criação faz milagres");
 
+           
+        }limpar();
 
-        //print("");
+        println("\033[3mFLASHBACK da sua adolescência...\033[0m");
+        println("Você discute com sua namorada e avança em cima dela.");
+        println("você levanta a mão, você vai bater nela?");
+        println("Sim ou Não?");
+        decisao = scan.nextLine();
+        if(decisao.equals("Sim")){
+            jogador.pontoMald();
+            println("Que horror!! Nem o inferno merece você");
+        }else{
+            jogador.pontoBond();
+            println("Ainda bem que você sabe como tratar uma dama!");
 
-        //pedir o nick
-        //plot da historinha: ser um julgamento a partir do nivel de maldade/bondade, com dois finais em que se for maior que 3 vai pro inferno, e menor que 3 vai pro céu
-        
-        //importar scanner - foi
-        //criar obj jogador
-        //extends classe jogador
-        //string da decisao - foi
-        //if e else
-        //nao esquecer de no final de usar o scanner, fechar ele com: scan.close();
+            
+        }limpar();
+      
+        println("\033[3mFLASHBACK da sua vida adulta...\033[0m");
+        println("Você na sua vida adulta teve um incrivel sucesso, e hoje em dia você tem uma boa condição financeira");
+        println("Uma ONG de cachorrinhos e gatinhos abandonados pede ajuda com doações pois eles ja não tinham o que comer. Você vai ajudar? ");
+        println("Sim ou Não");
+        decisao = scan.nextLine();
+        if(decisao.equals("Sim")){
+            jogador.pontoBond();
+            println("Você é uma pessoa incrivel e tem um bom coração!");
+        } else{
+            jogador.pontoMald();
+            println("Você é uma pessima pessoa!");
+            }
+            scan.close();
+
+            if(maldade > 3){
+                println("Você foi mandado para o inferno");
+            }else {
+                println("Você foi enviado ao céu" );
+            }
+
+        }
     }
-}
