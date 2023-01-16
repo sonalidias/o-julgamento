20c5b87d57ed54ad1c6e29278eb8dd3574dfaaf5
sonalidias <119533286+sonalidias@users.noreply.github.com>
2023-01-16 14:03:04 -0300

    Add files via upload

--- Jogador.java
+++ Jogador.java
@@ -4,8 +4,7 @@ public class Jogador extends utilidades {
         static double maldade = 0;
         double bondade;
     
-        public static void darNome(String nome){
-            println("Seu nome agora é " + nome);
+        public static void darNome(){
         }
     
         public static void pontoMald(double maldade){
